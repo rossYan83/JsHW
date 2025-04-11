192d512dab695142c88692f0e4e36b92f68d746f
Ros Yan <ros.yanikin@gmail.com>
2025-04-11 14:46:56 +0100

    new

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
+// const celsius = 100;
+// const fahrenheit = (celsius * 1.8) + 32;
+// console.log(fahrenheit);
 
-const celsius = 100;
-const fahrenheit = (celsius * 1.8) + 32;
-console.log(fahrenheit);
+// const daysInMonth = 30;
 
-const daysInMonth = 30;
+// const hoursInMonth = daysInMonth * 24;
+// console.log(hoursInMonth);
 
-const hoursInMonth = daysInMonth * 24;
-console.log(hoursInMonth);
+// const minutesInMonth = hoursInMonth * 60;
+// console.log(minutesInMonth)
 
-const minutesInMonth = hoursInMonth * 60;
-console.log(minutesInMonth)
+// let health = 0;
+// health --;
+// let energy = 0;
+// energy --;
 
-let health = 0;
-health --;
-let energy = 0;
-energy --;
+// const totalPrice = 10;
+// const discountPrice = totalPrice * 0.9;
+// console.log(discountPrice);
 
-const totalPrice = 10;
-const discountPrice = totalPrice * 0.9;
-console.log(discountPrice);
+// // const floatNumber = 12.78;
+// // const roundDown = Maths.floor(floatNumber);
+// // console.log(roundDown);
 
-// const floatNumber = 12.78;
-// const roundDown = Maths.floor(floatNumber);
-// console.log(roundDown);
+// const floatString = "45.67";
+// const parsedFloat = Number.parseFloat(floatString)
+// console.log(parsedFloat);
 
-const floatString = "45.67";
-const parsedFloat = Number.parseFloat(floatString)
-console.log(parsedFloat);
+// const intString = "123";
+// const parsedInt = Number.parseInt(intString);
+// console.log(parsedInt);
 
-const intString = "123";
-const parsedInt = Number.parseInt(intString);
-console.log(parsedInt);
+// const number = 8;
+// const sqrtNumber = Math.sqrt(8);
+// console.log(sqrtNumber)
 
-const number = 8;
-const sqrtNumber = Math.sqrt(8);
-console.log(sqrtNumber)
+// const integer = 42;
+// const stringNumber = "256";
 
-const integer = 42;
-const stringNumber = "256";
+// const convertedInt = parseInt(stringNumber);
+// console.log(convertedInt);
 
-const convertedInt = parseInt(stringNumber);
-console.log(convertedInt);
+// const convertedString = integer.toString();
+// console.log(convertedString);
 
-const convertedString = integer.toString();
-console.log(convertedString);  
+const result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+const email = "rostyslavYan@gmail.com";
+
+if (email.includes("@")) {
+  console.log("Електронна адреса містить символ '@'");
+} else {
+  console.log("Електронна адреса не містить символ '@'");
+}
+
+const word1 = "My";
+const word2 = "name";
+const word3 = "is";
+
+const fullName = word1 + " " + word2 + " " + word3 + " " + "Victor";
+console.log(fullName);
+
+const userName = "Charlie";
+const payment = 300;
+alert(`Дякуємо ${userName}, ! До сплати ${payment} гривень`);
