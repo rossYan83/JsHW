608e2cff880646eaa2af50c79d44d58bf7a19927
Ros Yan <ros.yanikin@gmail.com>
2025-05-02 16:33:17 +0100

    new

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ switch (drink) {
 }
 
 const dayOfWeek = "субота";
-dayOfWeek = dayOfWeek.toLowerCase();
+// dayOfWeek = dayOfWeek.toLowerCase();
 
 switch (dayOfWeek) {
   case "понеділок":
@@ -177,7 +177,7 @@ switch (month) {
 }
 
 const color = "red";
-color = color.toLowerCase();
+// color = color.toLowerCase();
 
 switch (color) {
   case "red":
@@ -215,4 +215,37 @@ switch (operator) {
     else{
       console.log("Division by zero is not allowed.");
     }
+}
+
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+for(let i = 2; i <= 20; i += 2){
+  console.log(i);
+}
+
+for(let i = 7; i <= 70; i+= 7){
+  console.log(i)
+}
+
+const n = 10;
+
+for(let i = 0; i <= 20; i++){
+  if(i >= n){
+    break;
+  }
+  console.log(i)
+}
+
+let num10= 1;
+while(num10 <= 20){
+  if(num10 % 3 === 0){
+    num10++;
+    continue;
+  }
+  console.log(num10);
+  num10++
 }
